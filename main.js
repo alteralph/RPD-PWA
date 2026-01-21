6a3f65eaa0de9f9ae3cb0797aaf18f7a652e7e77
gabsdfreitas <alteralph@proton.me>
2026-01-21 02:22:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,12 @@ const { app, BrowserWindow, screen } = require('electron');
 const path = require('path');
 
 function createWindow () {
-  // Pega o display principal ou onde o cursor está
   const primaryDisplay = screen.getPrimaryDisplay();
   const { width, height } = primaryDisplay.workAreaSize;
 
   const win = new BrowserWindow({
     width: 1200,
     height: 900,
-    // Centraliza manualmente baseado no tamanho do monitor
     x: Math.floor(primaryDisplay.bounds.x + (primaryDisplay.bounds.width - 1200) / 2),
                                 y: Math.floor(primaryDisplay.bounds.y + (primaryDisplay.bounds.height - 900) / 2),
                                 icon: path.join(__dirname, 'icon.png'),
@@ -17,7 +15,7 @@ function createWindow () {
                                   nodeIntegration: true,
                                 contextIsolation: false
                                 },
-                                autoHideMenuBar: true, // Esconde a barra de menu feia
+                                autoHideMenuBar: true,
                                 center: true
   });
 
